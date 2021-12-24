e2d8550c4ea80db82276c4da840f722d294e4da8
Natuuumi <56280643+Natuuumi@users.noreply.github.com>
2021-12-24 00:29:48 -0500

    added style

--- script.js
+++ script.js
@@ -1,11 +1,29 @@
+let body = document.querySelector("body");
+let myLibrary = ["peee", "eeee", "ttttt"];
+let displayBook;
 function Book(info) {
   this.info = () => {
     return "The Hobbit by J.R.R. Tolkien, 295 pages, not read yet";
   };
 }
 
+function addBookToLibrary(book) {
+  myLibrary.push(book.info());
+}
+
 const theHobbit = new Book(
   "The Hobbit by J.R.R. Tolkien, 295 pages, not read yet"
 );
 
+function displayEachBook(array) {
+  for (book in array) {
+    showBook = document.createElement("p");
+    showBook.textContent = array[book];
+    showBook.setAttribute("class", "bookStyle");
+    body.append(showBook);
+  }
+}
+
 console.log(theHobbit.info());
+
+displayEachBook(myLibrary);
